4f9489f38b5f199d0d027835189d01fc450a1f1d
Blake H <blake.hollingsworth80@gmail.com>
2023-08-02 17:17:37 -0600

    updated the install.js to actually prompt for the install of the app

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -3,18 +3,18 @@ const butInstall = document.getElementById('buttonInstall');
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    event.preventDefault();
-    installBtn.style.visibility = 'visible';
-});
+    // event.preventDefault();
+    butInstall.style.visibility = 'visible';
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {
-    event.prompt();
-    butInstall.setAttribute('disabled', true);
-    butInstall.textContent = 'Installed!';
+    // TODO: Implement a click event handler on the `butInstall` element
+    butInstall.addEventListener('click', async () => {
+        event.prompt();
+        butInstall.setAttribute('disabled', true);
+    });
 });
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {
     console.log('👍', 'appinstalled', event);
+    butInstall.textContent = 'Installed!';
 });
