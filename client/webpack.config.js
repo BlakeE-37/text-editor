ce637159df62bdafe1e4192d6941b7cf8239ad6a
Blake H <blake.hollingsworth80@gmail.com>
2023-08-02 16:49:29 -0600

    fixed webpack mainfest and now the app is installable

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -7,65 +7,61 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add CSS loaders and babel to webpack.
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './index.html',
-        title: 'Text Editor'
-      }),
-      new InjectManifest({
-        swSrc: './src-sw.js',
-        swDest: 'service-worker.js',
-      }),
-      new MiniCssExtractPlugin(),
-
-      new WebpackPwaManifest({
-        name: 'Text Editor',
-        short_name: 'JATE',
-        description: 'Edit code in a cool offline editor',
-        background_color: '#303030',
-        theme_color: '#102E4A',
-        start_url: './',
-        publicPath: './',
-        icons: [
-          {
-            src: path.resolve('./favicon.ico'),
-            sizes: [96],
-            destination: path.join('assets', 'icons'),
-          },
-        ],
-      }),
-
-    ],
-
-    module: {
-      rules: [
+module.exports = {
+  mode: 'development',
+  entry: {
+    main: './src/js/index.js',
+    install: './src/js/install.js'
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Text Editor'
+    }),
+    new InjectManifest({
+      swSrc: './src-sw.js',
+      swDest: 'service-worker.js',
+    }),
+    new WebpackPwaManifest({
+      name: 'Text Editor',
+      short_name: 'JATE',
+      description: 'Edit code in a cool offline editor',
+      background_color: '#303030',
+      theme_color: '#102E4A',
+      start_url: './',
+      publicPath: './',
+      icons: [
         {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: ['@babel/preset-env'],
-              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
-            },
-          },
+          src: path.resolve('./src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
         },
       ],
-    },
-  };
+    }),
+
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+          },
+        },
+      },
+    ],
+  },
 };
